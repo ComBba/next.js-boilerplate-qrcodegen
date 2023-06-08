285beee4e2e9d5de5dcea2eb5c1e01f49e917896
ComBba <cent@naver.com>
2023-06-08 21:35:12 +0900

    1. @headlessui/react의 Transition 사용 2. URL 검증 추가. 3. "styled-components" 버전 변경   - ^6.0.0-rc.3 -> ^5.3.11 4. datatable의 columns의 url을 함수형으로 변경 5. image 버튼으로 변경 6. 로딩시 Loading... 안내 하도록 수정. 7. API 리턴오류에 대한 안내 추가. 8. css 정리. 9. Source Cleaning.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-// pages/index.js
 import { useState, useEffect, useCallback } from 'react';
 import DataTable from 'react-data-table-component';
 import Image from 'next/image';
+import { Transition } from '@headlessui/react';
+
+const isValidURL = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (_) {
+    return false;
+  }
+};
 
 export default function Home() {
   const [url, setUrl] = useState('');
@@ -10,54 +19,88 @@ export default function Home() {
   const [totalRows, setTotalRows] = useState(0);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(5);
+  const [dialogInfo, setDialogInfo] = useState({ url: null, filename: null });
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleImageClick = (url, imageSrc) => {
+    setDialogInfo({ url, filename: imageSrc });
+    setDialogOpen(true);
+  };
+
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
 
   const columns = [
     {
       name: 'URL',
-      selector: 'url',
+      selector: row => row['url'],
       sortable: true,
       cell: row => <a href={row.url} target="_blank" rel="noreferrer">{row.url}</a>,
       width: '450px',
-      //grow: 2,  // This makes the URL column wider
     },
     {
       name: 'QR Code',
-      cell: row => <Image src={row.filename} alt="Generated QR Code" width={50} height={50} />,  // Reduced image size
+      cell: row => (
+        <div>
+          <button onClick={() => handleImageClick(row.url, row.filename)}>
+            <Image src={row.filename} alt="Generated QR Code" width={50} height={50} />
+          </button>
+        </div>
+      ),
     },
   ];
 
   const generateQR = async () => {
-    try {
-      const res = await fetch('/api/generate', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ url })
-      });
-      const { filename } = await res.json();
-      setFilename(filename);
-      await fetchHistory();
-    } catch (err) {
-      console.error(err);
+    setIsLoading(true);
+    setErrorMessage(null);
+    if (isValidURL(url)) {
+      try {
+        const res = await fetch('/api/generate', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ url })
+        });
+        if (!res.ok) {
+          setErrorMessage('Error generating QR code');
+          return;
+        }
+        const { filename } = await res.json();
+        setFilename(filename);
+        await fetchHistory();
+      } catch (err) {
+        console.error(err);
+        setErrorMessage('Error generating QR code');
+      }
+    } else {
+      setErrorMessage('Invalid URL');
     }
+    setIsLoading(false);
   }
 
   const fetchHistory = useCallback(async () => {
+    setIsLoading(true);
+    setErrorMessage(null);
     try {
       const res = await fetch(`/api/history?page=${page}&pageSize=${pageSize}`);
+      if (!res.ok) {
+        setErrorMessage('Error fetching history');
+        return;
+      }
       const data = await res.json();
       setHistory(data.qrHistory);
       setTotalRows(data.total);
     } catch (err) {
       console.error(err);
+      setErrorMessage('Error fetching history');
     }
+    setIsLoading(false);
   }, [page, pageSize]);
 
   useEffect(() => {
-    const loadData = async () => {
-      await fetchHistory();
-    };
-
-    loadData();
+    fetchHistory();
   }, [fetchHistory]);
 
   return (
@@ -65,40 +108,53 @@ export default function Home() {
       <div className="p-6 bg-white border-2 border-gray-300 rounded-md shadow-md dark:bg-gray-800 dark:text-white">
         <h2 className="mb-4 text-2xl">QR Code Generator</h2>
         <input
-          className="p-2 mb-2 border-2 border-gray-300 rounded-md dark:bg-gray-700 dark:text-white"
+          className="w-full p-2 mb-4 border-2 border-gray-300 rounded-md outline-none dark:border-gray-600 dark:bg-gray-700"
           type="text"
-          placeholder="Enter URL here"
+          placeholder="Enter URL"
           value={url}
-          onChange={e => setUrl(e.target.value)}
+          onChange={(e) => setUrl(e.target.value)}
         />
         <button
-          className="p-2 bg-blue-500 text-white rounded-md dark:bg-yellow-500 dark:text-black"
+          className="w-full p-2 mb-4 text-white bg-blue-500 rounded-md dark:bg-blue-400"
           onClick={generateQR}
         >
-          Generate
+          Generate QR Code
         </button>
-        {filename && (
+        {isLoading && (
           <div className="mt-4">
-            <Image src={filename} alt="Generated QR Code" width={200} height={200} />
+            Loading...
           </div>
         )}
-        <h2 className="mt-4 mb-2 text-xl">History</h2>
+        {errorMessage && (
+          <div className="mt-4 text-red-500">
+            {errorMessage}
+          </div>
+        )}
         <DataTable
+          title="Generated QR Code History"
           columns={columns}
           data={history}
+          progressPending={isLoading}
           pagination
-          //responsive
-          defaultSortField="url"
           paginationServer
-          paginationPerPage={pageSize}
+          onChangePage={setPage}
+          onChangeRowsPerPage={setPageSize}
           paginationTotalRows={totalRows}
-          onChangePage={page => setPage(page)}
-          onChangeRowsPerPage={(currentRowsPerPage, currentPage) => {
-            setPageSize(currentRowsPerPage);
-            setPage(currentPage);
-          }}
         />
       </div>
+      {dialogOpen && (
+        <Transition appear show={dialogOpen} as="div" className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white p-4 rounded shadow-xl">
+            <div className="flex justify-end">
+              <button onClick={closeDialog}>x</button>
+            </div>
+            <div className="mt-4">
+              <h2>URL: {dialogInfo.url}</h2>
+              <Image src={dialogInfo.filename} alt="Generated QR Code" width={200} height={200} />
+            </div>
+          </div>
+        </Transition>
+      )}
     </div>
   );
 }
